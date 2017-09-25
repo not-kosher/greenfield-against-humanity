62c7bafa4b5a56e1273875a5c24a2bbff9d0d609
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-09-25 13:48:35 -0700

    Refactor to not use namespace and write enterLobby function

--- server/socket/controllers/lobbyCtrl.js
+++ server/socket/controllers/lobbyCtrl.js
@@ -1,13 +1,21 @@
-const enterLobby = (game, client) => {
-  
+const enterLobby = (io, client) => {
+  client.join('lobby');
+  //send this only to the client who joined the room
+  client.emit('allRooms', 'rooms array here');
 };
 
-const createRoom = (game, client, roomname) => {
-
+const createRoom = (io, client, roomname) => {
+  //client creates a room 
+  //have logic on frontend to prevent an invalid roomname submission 
+  //(lobby, any existing room and no characters)
+  //this should create a new room (including a game object with alk data stored at key <roomname>),
+  //also set that user as the room creator 
+  //have the client leave the lobby, and join that room
 };
 
-const joinRoom = (game, client, roomname) => {
-
+const joinRoom = (io, client, roomname) => {
+  //client joins a room
+  //this should have the client leave the lobby and join the room (adding user to game object)
 };
 
 module.exports = {
--- server/socket/index.js
+++ server/socket/index.js
@@ -3,11 +3,10 @@ const lobbyRouter = require('./routes/lobbyRouter.js');
 
 const socketSetup = io => {
   io.listen(process.env.SOCKET || 8000);
-  const game = io.of('/game');
 
-  game.on('connection', client => {
-    gameRoomRouter(game, client);
-    lobbyRouter(game, client);
+  io.on('connection', client => {
+    gameRoomRouter(io, client);
+    lobbyRouter(io, client);
   });
 };
 
