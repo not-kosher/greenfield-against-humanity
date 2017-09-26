87c6b543e90782125c61bd90e37ffd780b734098
Philip Marazita <pmarazita@gmail.com>
2017-09-25 17:47:48 -0700

    Add socket connection on front end and temporary lobby ctrl functions

---
+++ client/src/socket/lobby.js
@@ -0,0 +1,28 @@
+import socket from './index.js';
+
+const lobbyCtrl = {
+  enterLobby: (cb) => {
+    socket.on('allRooms', (rooms) => {
+      cb(rooms);
+    });
+    socket.emit('enterLobby');
+  },
+  subscribeToRooms: (cb) => {
+    socket.on('newRoom', (room) => {
+      cb(room);
+    });
+  },
+  listenForJoinRoom: (cb) => {
+    socket.on('canJoinRoom', (room) => {
+      cb(room);
+    });
+  },
+  createRoom: (room) => {
+    socket.emit('createRoom', room);
+  },
+  joinRoom: (roomname, username) => {
+    socket.emit('joinRoom', roomname, username);
+  }
+};
+
+export default lobbyCtrl;
