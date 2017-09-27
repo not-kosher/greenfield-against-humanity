e939431d4f2516a7cd8ae85deaad86af4d8a2b8d
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-09-27 16:12:03 -0600

    Remove socket association with specific port
    This will allow for deployment on heroku

--- client/src/socket/index.js
+++ client/src/socket/index.js
@@ -1,9 +1,5 @@
-// called in app to open the socket and export it so both lobby and game room functions can access it
-// check for way to initialize socket without opening/closing socket
 import socketIOClient from 'socket.io-client';
 
-const endpoint = '127.0.0.1:8000';
-
-const socket = socketIOClient(endpoint);
+const socket = socketIOClient();
 
 export default socket;
