5688559f024ff85302d43ef8b47ab80943ff7602
Philip Marazita <pmarazita@gmail.com>
2017-09-27 22:20:56 -0700

    Convert dashboard component to functional stateless

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,33 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-class Dashboard extends React.Component {
-  constructor() {
-    super();
-  }
-
-  render() {
-    return (
-      <div className='Dashboard'>
-        <div>
-          <div className='Logo'>Greenfield Against Humanity</div>
-          <div className='dashboardContent'> 
-            <div className='Title'>Welcome, you filthy cur</div>
-            <div>
-              <Link to='lobby'>
-                Play a game
-              </Link>
-            </div>
-            <div>
-              <Link to='deckbuilder'>
-                Build a deck
-              </Link>
-            </div>
+const Dashboard = (props) => {
+  return (
+    <div className='Dashboard'>
+      <div>
+        <div className='Logo'>Greenfield Against Humanity</div>
+        <div className='dashboardContent'> 
+          <div className='Title'>Welcome, you filthy cur</div>
+          <div>
+            <Link to='lobby'>
+              Play a game
+            </Link>
+          </div>
+          <div>
+            <Link to='deckbuilder'>
+              Build a deck
+            </Link>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Dashboard;
