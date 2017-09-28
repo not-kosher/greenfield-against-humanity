5749d55e842d1da5e1fea19d96af067702b59fd3
Philip Marazita <pmarazita@gmail.com>
2017-09-27 21:41:27 -0700

    Rearrange files and routes to prepare for user login routing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Dashboard from './Dashboard/Dashboard';
+import Landing from './Landing/Landing';
 import Lobby from './Lobby/Lobby';
 import GameRoom from './GameRoom/GameRoom';
+import DeckBuilder from './DeckBuilder/DeckBuilder';
 
 class App extends React.Component {
   constructor() {
@@ -13,8 +16,11 @@ class App extends React.Component {
     return (
       <div className='App'>
         <Switch>
-          <Route exact path='/' component={Lobby} />
+          {/* <Route exact path='/' component={Landing} /> */}
+          <Route exact path='/' component={Dashboard} />
+          <Route path='/lobby' component={Lobby} />
           <Route path='/game/:room/:username' component={GameRoom} />
+          <Route path='/deckbuilder' component={DeckBuilder} />
         </Switch>
       </div>
     );
