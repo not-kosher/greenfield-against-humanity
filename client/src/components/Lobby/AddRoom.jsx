2e85239df22ced243b4c0d81cb88ee89be5b4d84
Philip Marazita <pmarazita@gmail.com>
2017-10-01 15:15:11 -0700

    Adjust styling for add room form

--- client/src/components/Lobby/AddRoom.jsx
+++ client/src/components/Lobby/AddRoom.jsx
@@ -4,18 +4,22 @@ const AddRoom = ({ createRoom }) => {
   return (
     <div className='AddRoom'>
       <div className='ClassFormContainer'>
-        <div>Create A Room:</div>
-        <form onSubmit={createRoom}>
-          <div>
-            <input id='roomname' placeholder='Enter room name'/>
+        <div className='form-title'>Create A Room:</div>
+        <form onSubmit={createRoom} className='add-room-form'>
+          <div className='addroom-input-container'>
+            <input id='roomname' placeholder='Enter room name' className='addroom-input'/>
           </div>
-          <div>
-            <label>Select a Deck:  </label>
-            <select id='deckname' >
+          <div className='addroom-input-container'>
+            <label className='addroom-label'>Points to win:  </label>
+            <input type='number' id='points-to-win' className='addroom-input' placeholder='10'/>
+          </div>
+          <div className='addroom-input-container'>
+            <label className='addroom-label'>Select decks:  </label>
+            <select id='deckname' className='addroom-input'>
               <option>Base Set</option>
             </select>  
           </div>
-          <div>
+          <div className='addroom-input-container'>
             <input type='submit' value='Submit' className='submit-button'/>
           </div>
         </form>
