be27d32af8d3564ab87f4b8b5709cfd751f12ed6
Philip Marazita <pmarazita@gmail.com>
2017-10-01 13:41:33 -0700

    Adjust db index and seed file to eventually work with custom decks

--- server/db/seeding/seed.js
+++ server/db/seeding/seed.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const data = require('./basecards.json');
 const { WhiteCard, BlackCard, Deck } = require('../index.js');
 
@@ -12,16 +13,18 @@ Deck.sync({ force: true })
     return WhiteCard.sync({ force: true });
   })
   .then(() => {
-    return BlackCard.sync({ force: true });
-  })
-  .then(() => {
     const whites = data.whiteCards.map((text) => {
       return { text, deckId: 1 };
     });
+    return WhiteCard.bulkCreate(whites);
+  })
+  .then(() => {
+    return BlackCard.sync({ force: true });
+  })
+  .then(() => {
     const blacks = data.blackCards.map((card) => {
       card.deckId = 1;
       return card;
     });
-    WhiteCard.bulkCreate(whites);
     BlackCard.bulkCreate(blacks);
   });
