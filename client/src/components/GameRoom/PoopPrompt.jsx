c78a75c24bfafff5f2ca3fd96605ab6f48eaf62c
Tyler Vander Maas <tvmaasjazz@gmail.com>
2017-10-02 12:05:28 -0700

    Add styling to the game room
    Fixed poop prompt positioning, fixed border on alerts and
    made alert buttons stand out

--- client/src/components/GameRoom/PoopPrompt.jsx
+++ client/src/components/GameRoom/PoopPrompt.jsx
@@ -7,7 +7,7 @@ const PoopPrompt = ({ poopSubmission }) => {
         <div id='waitingOnPoopers'>Waiting for all players to submit</div>
         <div id='prompt'>
           <div className='poopQ'>How many hours has it been since you last pooped?</div>
-          <input id='poopHours' />
+          <input id='poopHours' type='number'/>
           <div className='poopSubmit' onClick={poopSubmission}>Submit</div>
         </div>
       </div>
