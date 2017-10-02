8389f5b4d70e7a9dbea73265f9b42b492ee62602
Philip Marazita <pmarazita@gmail.com>
2017-10-01 18:34:57 -0700

    Modify layout and styling for game room components

--- client/src/components/GameRoom/MessageInput.jsx
+++ client/src/components/GameRoom/MessageInput.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MessageInput = ({submitMessage}) => (
   <form action="#" className="message-input-form" onSubmit={submitMessage}>
-    <input className="message-input" id="message-input" type="text" />
+    <input className="message-input" id="message-input" type="text" placeholder='Send a message'/>
     <button className="message-input-button">Send</button>
   </form>
 );
