bdcea1a4a907435f2c629733d07a9c9e0e03762e
Philip Marazita <pmarazita@gmail.com>
2017-10-02 14:54:08 -0700

    Add default value when not entered

--- client/src/components/Lobby/Lobby.jsx
+++ client/src/components/Lobby/Lobby.jsx
@@ -45,8 +45,8 @@ class Lobby extends React.Component {
     // TODO needs data validation for room name
     e.preventDefault();
     const roomname = document.getElementById('roomname').value;
-    const pointsToWin = document.getElementById('points-to-win').value;
     const deckname = document.getElementById('deckname').value;
+    console.log('Points to win', pointsToWin);
     socket.emit('createRoom', roomname, this.props.username, deckname, pointsToWin);
   }
 
